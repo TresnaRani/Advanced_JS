6b77a52b9b6ef519bfbba1d88868c20ccd7c4540
TresnaRani <tresna3122gmail.com>
2022-12-29 14:42:54 +0600

    initial commit

---
+++ DOM.js
@@ -0,0 +1,8 @@
+
+document.getElementById("heading").innerHTML = "More power"
+
+var mypara = document.getElementById("para");
+mypara.innerHTML="Now my time";
+
+var head = document.getElementById("head");
+head.innerHTML="no more chance"
