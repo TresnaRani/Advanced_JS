2b8f7bf47d91ca62032e458d0f2e8db4c7b72160
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-01-04 15:14:39 +0600

    Style

---
+++ Style.js
@@ -0,0 +1,13 @@
+
+
+function addStyle(){
+
+     var myvar=document.querySelector("#para");
+     myvar.classList.add("para-style");
+}
+
+function removeStyle(){
+
+    var myvar = document.querySelector("#para");
+    myvar.classList.remove("para-style");
+}
