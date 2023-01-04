532bafc551e3b758ee00dac2e53d31ebd1f16bb9
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-01-04 16:54:04 +0600

    Event

---
+++ EventListener.js
@@ -0,0 +1,35 @@
+
+ 
+ 
+ document.querySelector("button").addEventListener("click",myfuntion);
+
+
+
+
+ function myfuntion(){
+
+
+   alert("hello")
+}
+
+
+
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseover",function(){
+
+
+    myVar.classList.add("my-style");
+});
+
+
+myVar.addEventListener("mouseout",function(){
+
+
+    myVar.classList.remove("my-style");
+});
+
+
+
+
+
